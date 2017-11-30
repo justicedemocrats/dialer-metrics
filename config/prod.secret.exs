57d0d1b63880cf57c039b868002eac28127a2bbb
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-11-30 00:15:22 -0600

    production config is back, omg

---
+++ config/prod.secret.exs
@@ -0,0 +1,25 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you'll likely want to automate and keep away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or yourself later on).
+config :livevox, LivevoxWeb.Endpoint,
+  secret_key_base: "BitDdVAQqq8jPp0KBcrU+wyAji1Rn3/X1E51VfBJVpl6ensSM5tYQiwMlAyqGFCa"
+
+config :livevox,
+  access_token: "40c206f4423442b0b76d39796498123d",
+  clientname: "brandnewcampaign",
+  username: "sbriggs",
+  password: "T571tKjKaP6B6jHsDi"
+
+config :livevox,
+  data_dog_api_key: "451d1cce6d3a588ac51bb79f710d2d3d",
+  data_dog_application_key: "b5d4c4edf1844a9cb34c9f654e9355d348d8e308",
+  airtable_key: "keylEG1YySJXMRa6Z",
+  airtable_base: "appYsMtTeDehJmTJb",
+  airtable_table_name: "Term%20Codes"
