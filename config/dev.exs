c912e72ff0dbe0912fcc0552e1a2b913a62340c9
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-03 19:04:05 -0500

    no rollbax in dev mode, also update deps

--- config/dev.exs
+++ config/dev.exs
@@ -31,6 +31,7 @@ config :livevox, LivevoxWeb.Endpoint,
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+config :rollbax, enabled: false
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
