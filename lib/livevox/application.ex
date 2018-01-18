bae8d1c5ee432e00c12c07ca5498c8510e14c6a1
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-18 16:45:24 -0500

    only do call-results

--- lib/livevox/application.ex
+++ lib/livevox/application.ex
@@ -34,21 +34,21 @@ defmodule Livevox.Application do
       worker(Livevox.CallEventFeed, []),
 
       # Metrics
-      worker(Livevox.Metrics.CallerCounts, []),
-      worker(Livevox.Metrics.CallCounts, []),
-      worker(Livevox.Metrics.ServiceLevel, []),
-      worker(Livevox.Metrics.WaitTime, []),
-      worker(Livevox.Metrics.SessionLength, []),
-      worker(Livevox.Metrics.CallLength, []),
+      # worker(Livevox.Metrics.CallerCounts, []),
+      # worker(Livevox.Metrics.CallCounts, []),
+      # worker(Livevox.Metrics.ServiceLevel, []),
+      # worker(Livevox.Metrics.WaitTime, []),
+      # worker(Livevox.Metrics.SessionLength, []),
+      # worker(Livevox.Metrics.CallLength, []),
 
       # Event loggers
-      worker(Livevox.EventLoggers.CallEvent, []),
-      worker(Livevox.EventLoggers.AgentEvent, []),
-      # worker(Livevox.EventLoggers.CallResult, []),
+      # worker(Livevox.EventLoggers.CallEvent, []),
+      # worker(Livevox.EventLoggers.AgentEvent, []),
+      worker(Livevox.EventLoggers.CallResult, [])
 
       # Aggregators
-      worker(Livevox.Aggregators.ServiceConfig, []),
-      worker(Livevox.Aggregators.AgentStatus, [])
+      # worker(Livevox.Aggregators.ServiceConfig, []),
+      # worker(Livevox.Aggregators.AgentStatus, [])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
