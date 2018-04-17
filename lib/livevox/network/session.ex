185562b9a0911f72a4d0619f61c03e678d2f91ce
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-04-17 01:54:29 -0400

    fix env var naming

--- lib/livevox/network/session.ex
+++ lib/livevox/network/session.ex
@@ -3,9 +3,9 @@ defmodule Livevox.Session do
   defstruct [:id, :expires_at]
   require Logger
 
-  def clientname, do: Application.get_env(:justice_dialer, :livevox_clientname)
-  def username, do: Application.get_env(:justice_dialer, :livevox_username)
-  def password, do: Application.get_env(:justice_dialer, :livevox_password)
+  def clientname, do: Application.get_env(:livevox, :clientname)
+  def username, do: Application.get_env(:livevox, :username)
+  def password, do: Application.get_env(:livevox, :password)
 
   # State takes the format index
   def start_link do
